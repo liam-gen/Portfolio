62c7e9107a1e9a3760db11f7555d2de26bb2ceab
liamgen <liamgen.js@proton.me>
2023-09-02 21:14:56 +0200

    v1.0.0

--- cdn/js/script.js
+++ cdn/js/script.js
@@ -35,3 +35,14 @@ document.querySelectorAll("*:not(a)").forEach(e => {
         }
     }
 })
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    setTimeout(() => {
+        document.querySelector(".loader").style.animation = "out 2s"
+        document.querySelector(".container").style.display = "block"
+        
+    }, 1000)
+    setTimeout(() => {
+        document.querySelector(".loader").style.display = "none"
+    }, 2000)
+});
